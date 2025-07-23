137eda336cf656eaf1cbe32ff6ab32922411e68f
FelixJemlich <felix.jemlich@gmail.com>
2025-07-23 20:16:54 +0200

    implement logic

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
-let imgTitle = [
-    Church-1-min.jpg,
-    Church-2-min.jpg,
-    Church-3-min.jpg,
-    Church-4-min.jpg,
-    Church-5-min.jpg,
-    Church-6-min.jpg,
-    Church-7-min.jpg,
-    Church-8-min.jpg,
-    Church-9-min.jpg,
-    Church-10-min.jpg,
-    Church-11-min.jpg,
-    Church-12-min.jpg
-];
+//ids vom dom holen
+function churchId(ids){
 
-let imgNumber = [
-
-]
+    let ids=[];
+    
+    for (let i = 1; i <= 12; i++) {
+        ids.push(`church-${i}`);
+    }
+    return ids;
+}
